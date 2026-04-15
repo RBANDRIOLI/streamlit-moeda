ce6f106070fe29a1d98491a04bdb4dfaef73ea6e
RBANDRIOLI <andrioli_alemoa@hotmail.com>
2026-04-15 01:25:27 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 {\rtf1\ansi\ansicpg1252\cocoartf2869
 \cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red203\green35\blue57;\red0\green0\blue0;\red0\green0\blue0;
-\red6\green33\blue79;}
-{\*\expandedcolortbl;;\cssrgb\c84314\c22745\c28627;\cssrgb\c0\c0\c0\c7843;\cssrgb\c0\c0\c0\c83137;
-\cssrgb\c1176\c18431\c38431;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0\c83137;\cssrgb\c0\c0\c0\c7843;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
 \pard\pardeftab720\partightenfactor0
 
 \f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import\cf4 \strokec4  streamlit \cf2 \strokec2 as\cf4 \strokec4  st\
+import streamlit as st\
 \
-st.header(\cf5 \strokec5 'Jogando uma moeda'\cf4 \strokec4 )\
+st.header(\'91Jogando uma moeda\'92)\
 \
-st.write(\cf5 \strokec5 'Ainda n\'e3o \'e9 um aplicativo funcional. Em constru\'e7\'e3o.'\cf4 \strokec4 )}
+st.write(\'91Ainda n\'e3o \'e9 um aplicativo funcional. Em constru\'e7\'e3o.\'92)}
